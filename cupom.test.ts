95854d910d5a219263c4d7c2d3cf806039d2a429
Rodrigo Vilar <rodrigovilar@dcx.ufpb.br>
2020-09-20 20:29:46 -0300

    Testes para o Exercício 5

--- cupom.test.ts
+++ cupom.test.ts
@@ -1,5 +1,13 @@
 const cupom = require('./cupom');
 
+function verificaCampoObrigatorio(mensagemEsperada: string) {
+  try {
+    cupom.dados_loja();
+  } catch (e) {
+    expect(e.message).toBe(mensagemEsperada);
+  }
+}
+
 test('Loja Completa', () => {
   expect(cupom.dados_loja()).toBe(
     `Arcos Dourados Com. de Alimentos LTDA
@@ -14,21 +22,13 @@ IE: 244.898.500.113
 
 test('Nome vazio', () => {
   cupom.dados.nome_loja = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo nome da loja é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo nome da loja é obrigatório`);
   cupom.dados.nome_loja = "Arcos Dourados Com. de Alimentos LTDA";
 });
 
 test('Logradouro vazio', () => {
   cupom.dados.logradouro = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo logradouro do endereço é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo logradouro do endereço é obrigatório`);
   cupom.dados.logradouro = "Av. Projetada Leste";
 });
 
@@ -48,41 +48,25 @@ IE: 244.898.500.113
 
 test('Município vazio', () => {
   cupom.dados.municipio = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo município do endereço é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo município do endereço é obrigatório`);
   cupom.dados.municipio = "Campinas";
 });
 
 test('Estado vazio', () => {
   cupom.dados.estado = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo estado do endereço é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo estado do endereço é obrigatório`);
   cupom.dados.estado = "SP";
 });
 
 test('CNPJ vazio', () => {
   cupom.dados.cnpj = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo CNPJ da loja é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo CNPJ da loja é obrigatório`);
   cupom.dados.cnpj = "42.591.651/0797-34";
 });
 
 test('Inscrição estadual vazia', () => {
   cupom.dados.inscricao_estadual = "";
-  try {
-    cupom.dados_loja();
-  } catch (e) {
-    expect(e.message).toBe(`O campo inscrição estadual da loja é obrigatório`);
-  }
+  verificaCampoObrigatorio(`O campo inscrição estadual da loja é obrigatório`);
   cupom.dados.inscricao_estadual = "244.898.500.113";
 });
 
